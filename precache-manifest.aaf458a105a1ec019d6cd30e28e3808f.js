50ad719a83dff855319ab47ea03eb69e2da36daa
sandeep-07 <sandeepisthename007@gmail.com>
2021-08-02 16:37:16 +0530

    Updates

---
+++ precache-manifest.aaf458a105a1ec019d6cd30e28e3808f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffedeb87676d5b523515eb898bfae8fe",
+    "url": "/osi-digi/index.html"
+  },
+  {
+    "revision": "aa6496f70492cf26d9ad",
+    "url": "/osi-digi/static/css/2.afb34292.chunk.css"
+  },
+  {
+    "revision": "379700374eddeb232222",
+    "url": "/osi-digi/static/css/main.01e51f89.chunk.css"
+  },
+  {
+    "revision": "aa6496f70492cf26d9ad",
+    "url": "/osi-digi/static/js/2.197910da.chunk.js"
+  },
+  {
+    "revision": "487813aa5328b93fe53a4ae5cdb28065",
+    "url": "/osi-digi/static/js/2.197910da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379700374eddeb232222",
+    "url": "/osi-digi/static/js/main.06720d89.chunk.js"
+  },
+  {
+    "revision": "905de37ee44f8dcfa34c",
+    "url": "/osi-digi/static/js/runtime-main.90e89661.js"
+  }
+]);
